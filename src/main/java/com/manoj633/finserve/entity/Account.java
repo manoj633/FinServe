d085fe8c5f5560efd05e9955ce4a899c1c006cb2
Manoj <manoj09633@gmail.com>
2024-06-22 22:12:20 +0530

    Created Account Entity

---
+++ src/main/java/com/manoj633/finserve/entity/Account.java
@@ -0,0 +1,27 @@
+package com.manoj633.finserve.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "account_holder_name")
+    private String accountHolderName;
+
+    @Column(name = "balance")
+    private Double balance;
+}
