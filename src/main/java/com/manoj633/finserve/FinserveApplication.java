90fdefc454a5d6ad145194a6255a026e44caca44
Manoj <manoj09633@gmail.com>
2024-06-22 21:59:34 +0530

    Initial commit

---
+++ src/main/java/com/manoj633/finserve/FinserveApplication.java
@@ -0,0 +1,13 @@
+package com.manoj633.finserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinserveApplication.class, args);
+	}
+
+}
