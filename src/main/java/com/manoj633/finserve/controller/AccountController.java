f4037abe1240869e7708aae4ee27ccc2d7b5bdcf
Manoj <manoj09633@gmail.com>
2024-06-25 19:55:03 +0530

    Added REST endpoint to controller for get account by ID

--- src/main/java/com/manoj633/finserve/controller/AccountController.java
+++ src/main/java/com/manoj633/finserve/controller/AccountController.java
@@ -4,10 +4,7 @@ import com.manoj633.finserve.dto.AccountDto;
 import com.manoj633.finserve.service.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -22,5 +19,12 @@ public class AccountController {
     @PostMapping
     public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto) {
         return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+    }
+
+//    Account by ID REST API
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDto> getAccountById(@PathVariable  Long id) {
+        AccountDto accountDto = accountService.getAccountById(id);
+        return ResponseEntity.ok(accountDto);
     }
 }
