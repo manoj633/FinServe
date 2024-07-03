f9a1dab49cc9f4a4ea643f58c2f8967e4beffae3
Manoj <manoj09633@gmail.com>
2024-07-03 20:17:21 +0530

    created service method to get all accounts

--- src/main/java/com/manoj633/finserve/service/AccountService.java
+++ src/main/java/com/manoj633/finserve/service/AccountService.java
@@ -3,6 +3,8 @@ package com.manoj633.finserve.service;
 import com.manoj633.finserve.dto.AccountDto;
 import com.manoj633.finserve.entity.Account;
 
+import java.util.List;
+
 public interface AccountService {
 
     AccountDto createAccount(AccountDto accountDto);
@@ -12,4 +14,6 @@ public interface AccountService {
     AccountDto deposit(Long id, double amount);
 
     AccountDto withdraw(Long id, double amount);
+
+    List<AccountDto> getAllAccounts();
 }
--- src/main/java/com/manoj633/finserve/service/impl/AccountServiceImpl.java
+++ src/main/java/com/manoj633/finserve/service/impl/AccountServiceImpl.java
@@ -7,6 +7,10 @@ import com.manoj633.finserve.repository.AccountRepository;
 import com.manoj633.finserve.service.AccountService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 @Service
 public class AccountServiceImpl implements AccountService {
 
@@ -48,5 +52,11 @@ public class AccountServiceImpl implements AccountService {
         account.setBalance(total);
         Account savedAccount = accountRepository.save(account);
         return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
+    @Override
+    public List<AccountDto> getAllAccounts() {
+        List<Account> accounts = accountRepository.findAll();
+        return accounts.stream().map((account) -> AccountMapper.mapToAccountDto(account)).collect(Collectors.toList());
     }
 }
